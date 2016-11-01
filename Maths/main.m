0a22a8d7e4a58ac6a793e38c1d52255320a579f9
ttjnguyen <ttjnguyen6@gmail.com>
2016-11-01 12:11:11 -0700

    Initial Commit

---
+++ Maths/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Maths
+//
+//  Created by Jenny Nguyen on 2016-11-01.
+//  Copyright © 2016 LightHouseLabs. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
